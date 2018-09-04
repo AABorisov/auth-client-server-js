73ac55938e61780791aadf5b8ed2ba69d975b257
AABorisov <borisov.andrew.a@gmail.com>
2018-09-04 07:26:17 +0300

    Client Setup:     axios     react     react-dom     react-redux     react-router-dom     react-scripts     redux     redux-form     redux-thunk
    Pages:
        Header
        Welcome
        Signup

---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Signup extends Component {
+    render() {
+        return (
+            <form>
+                <fieldset>
+                    <label>Email</label>
+                    <input/>
+                </fieldset>
+                <fieldset>
+                    <label>Password</label>
+                    <input/>
+                </fieldset>
+            </form>
+        );
+    }
+}
